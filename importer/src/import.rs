ff5d1ba3f63fa18bddede53d2e6c5cf0115b912b
Jason Chen <jason@jcndrop.com>
2020-03-07 22:13:56 -0800

    Add minimum working import module

--- importer/src/import.rs
+++ importer/src/import.rs
@@ -1,37 +1,78 @@
-use crate::av::metadata::{MetadataValue, Track};
-use crate::metadata::providers::MBClient;
+use regex::Regex;
+
+use crate::av::metadata::{MetadataValue, Track as AVTrack};
+use crate::metadata::providers::{MBClient, SpotifyClient};
 use crate::metadata::providers::musicbrainz::SearchResult;
 use crate::metadata::providers::musicbrainz::entities;
 use crate::utils::lev::damlev;
 
+use crate::models::*;
+
 pub struct TrackImporter<'a> {
-    client: MBClient,
-    track: Track<'a>,
+    mb_client: MBClient,
+    spotify_client: SpotifyClient,
+    track: AVTrack<'a>,
 }
 
 impl<'a> TrackImporter<'a> {
-    pub fn new(client: MBClient, track: Track<'a>) -> TrackImporter<'a> {
+    pub fn new(mb_client: MBClient, spotify_client: SpotifyClient, track: AVTrack<'a>) -> TrackImporter<'a> {
         TrackImporter {
-            client,
+            mb_client,
+            spotify_client,
             track,
         }
     }
 
-    pub async fn import(&self) {
+    pub async fn import(&self) -> (Artist, Album, Track) {
         let rec = self.match_to_recording().await.unwrap();
         let release = match rec.releases.as_ref() {
             Some(releases) => self.match_release(&releases),
             _ => None,
         }.unwrap();
         let artist_credit = release.artist_credit.as_ref().unwrap().first().unwrap();
-        let artist = self.client.get_artist(&artist_credit.artist.id).await.unwrap();
-        println!("Release :{:#?}", release);
-        println!("Recording:{:#?}", rec);
-        println!("Artist:{:#?}", artist);
+        let artist = self.mb_client.get_artist(&artist_credit.artist.id).await.unwrap();
+        let spotify_artist = match find_artist_spotify_id(&artist.relations) {
+            Some(id) => Some(self.spotify_client.get_artist(&id).await.unwrap()),
+            None => None,
+        };
+        let cover_art = self.mb_client.get_cover_art(&release.id).await.unwrap();
+
+        let artist_image = spotify_artist.and_then(|a| {
+            a.images.iter().max_by_key(|i| i.width.unwrap_or(0)).map(|i| i.url.clone())
+        });
+
+        let album_image = cover_art.images.iter().filter(|i| i.front).next().map(|i| i.image.clone());
+
+        let artist = Artist {
+            mbid: artist_credit.artist.id.clone(),
+            name: artist_credit.artist.name.clone(),
+            image_url: artist_image,
+        };
+
+        let album = Album {
+            mbid: release.id.clone(),
+            name: release.title.clone(),
+            image_url: album_image,
+        };
+
+        let position = release.media.first().as_ref()
+            .map(|m| {
+                m.track.first().as_ref().map(|t| u16::from_str_radix(&t.number, 10).unwrap()).unwrap()
+            })
+            .unwrap();
+        let track = Track {
+            mbid: rec.id.unwrap(),
+            title: rec.title.unwrap(),
+            position,
+            bitrate: self.track.bit_rate(),
+            duration: self.track.duration(),
+        };
+
+        (artist, album, track)
     }
 
     pub async fn search_recordings(&self) -> Vec<entities::Recording> {
-        let res = self.client.search_recordings(&self.track).await.unwrap();
+        let res = self.mb_client.search_recordings(&self.track).await.unwrap();
         match res.results {
             SearchResult::Recordings(r) => r,
             _ => panic!("unexpected deserialization result"),
@@ -139,4 +180,23 @@ impl<'a> TrackImporter<'a> {
         .min_by_key(|(score, _)| *score)
         .map(|(_, r)| r)
     }
+}
+
+fn find_artist_spotify_id(relations: &Vec<entities::Relation>) -> Option<String> {
+    let reg = Regex::new(r"^https://open\.spotify\.com/artist/([a-zA-Z0-9-_]+)$").unwrap();
+    for r in relations {
+        match &r.url {
+            Some(r) => {
+                let caps = reg.captures(&r.resource);
+                match caps {
+                    Some(caps) => {
+                        return Some(caps.get(1).unwrap().as_str().to_string());
+                    }
+                    None => {}
+                }
+            }
+            None => {}
+        }
+    }
+    None
 }

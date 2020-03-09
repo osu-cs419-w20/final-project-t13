0989d85d006257806ad1f67f81d19fae68273be8
Jason Chen <jason@jcndrop.com>
2020-03-09 14:30:05 -0700

    Make import module more modular

--- importer/src/import.rs
+++ importer/src/import.rs
@@ -24,38 +24,17 @@ impl<'a> TrackImporter<'a> {
         }
     }
 
-    pub async fn import(&self) -> (Artist, Album, Track) {
+    pub async fn find_match(&self) -> (entities::ArtistCredit, entities::Release, entities::Recording) {
         let rec = self.match_to_recording().await.unwrap();
         let release = match rec.releases.as_ref() {
             Some(releases) => self.match_release(&releases),
             _ => None,
         }.unwrap();
-        let artist_credit = release.artist_credit.as_ref().unwrap().first().unwrap();
-        let artist = self.mb_client.get_artist(&artist_credit.artist.id).await.unwrap();
-        let spotify_artist = match find_artist_spotify_id(&artist.relations) {
-            Some(id) => Some(self.spotify_client.get_artist(&id).await.unwrap()),
-            None => None,
-        };
-        let cover_art = self.mb_client.get_cover_art(&release.id).await.unwrap();
+        let artist_credit = release.artist_credit.as_ref().unwrap().first().unwrap().clone();
+        (artist_credit, release, rec)
+    }
 
-        let artist_image = spotify_artist.and_then(|a| {
-            a.images.iter().max_by_key(|i| i.width.unwrap_or(0)).map(|i| i.url.clone())
-        });
-
-        let album_image = cover_art.images.iter().filter(|i| i.front).next().map(|i| i.image.clone());
-
-        let artist = Artist {
-            mbid: artist_credit.artist.id.clone(),
-            name: artist_credit.artist.name.clone(),
-            image_url: artist_image,
-        };
-
-        let album = Album {
-            mbid: release.id.clone(),
-            name: release.title.clone(),
-            image_url: album_image,
-        };
-
+    pub fn build_track(&self, rec: &entities::Recording, release: &entities::Release) -> Track {
         let position = release.media.first().as_ref()
             .map(|m| {
                 m.track.first().as_ref().map(|t| u16::from_str_radix(&t.number, 10).unwrap()).unwrap()
@@ -64,15 +43,80 @@ impl<'a> TrackImporter<'a> {
         let file_location = self.track.path_str().unwrap().to_string();
         let file_location = file_location.as_str().trim_start_matches("/Users/jason/j/tmp/dtst");
         const FRAGMENT: &AsciiSet = &CONTROLS.add(b' ').add(b'"').add(b'<').add(b'>').add(b'`');
-        let track = Track {
-            mbid: rec.id.unwrap(),
-            title: rec.title.unwrap(),
+
+        Track {
+            mbid: rec.id.clone().unwrap(),
+            title: rec.title.clone().unwrap(),
             position,
             bitrate: self.track.bit_rate(),
             duration: self.track.duration(),
             file_location: utf8_percent_encode(&format!("/static{}", file_location), FRAGMENT).to_string(),
+        }
+    }
+
+    pub async fn build_artist(&self, artist_credit: &entities::ArtistCredit) -> Artist {
+        let artist = self.mb_client.get_artist(&artist_credit.artist.id).await.unwrap();
+        let spotify_artist = match find_artist_spotify_id(&artist.relations) {
+            Some(id) => Some(self.spotify_client.get_artist(&id).await.unwrap()),
+            None => None,
+        };
+        let artist_image = spotify_artist.and_then(|a| {
+            a.images.iter().max_by_key(|i| i.width.unwrap_or(0)).map(|i| i.url.clone())
+        });
+
+        Artist {
+            mbid: artist_credit.artist.id.clone(),
+            name: artist_credit.artist.name.clone(),
+            image_url: artist_image,
+        }
+    }
+
+    pub async fn build_album(&self, release: &entities::Release, artist_credit: &entities::ArtistCredit) -> Album {
+        // Check if Covert Art Archive has this release
+        let image_url = match self.mb_client.get_cover_art(&release.id).await.unwrap() {
+            Some(cover_art) => {
+                cover_art.images
+                    .iter()
+                    .filter(|i| i.front)
+                    .next()
+                    .map(|i| i.image.clone())
+            }
+            // If release is not on Covert Art Archive, try the Spotify API
+            None => {
+                match self.spotify_client.search_album(&release.title, &artist_credit.artist.name).await {
+                    Ok(albums) => {
+                        albums
+                            .into_iter()
+                            .map(|a| (damlev(&a.name, &release.title), a))
+                            .min_by_key(|(s, _)| *s)
+                            .and_then(|(_, a)| {
+                                a.images
+                                    .into_iter()
+                                    .max_by_key(|i| i.width.unwrap_or(0))
+                                    .map(|i| i.url)
+                            })
+                    }
+                    _ => None
+                }
+            }
         };
 
+        Album {
+            mbid: release.id.clone(),
+            name: release.title.clone(),
+            image_url,
+        }
+    }
+
+    pub async fn import(&self) -> (Artist, Album, Track) {
+        let (artist_credit, release, rec) = self.find_match().await;
+        self.from_entities(&rec, &release, &artist_credit).await
+    }
+
+    pub async fn from_entities(&self, rec: &entities::Recording, release: &entities::Release, artist_credit: &entities::ArtistCredit) -> (Artist, Album, Track) {
+        let artist = self.build_artist(artist_credit).await;
+        let album = self.build_album(release, artist_credit).await;
+        let track = self.build_track(rec, release);
         (artist, album, track)
     }
 

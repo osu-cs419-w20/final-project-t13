2347fc5116502fd48e504f2c3c7149353e83c434
Jason Chen <jason@jcndrop.com>
2020-03-16 16:48:07 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import Modal from 'react-modal'
+
+import App from './app'
+import store from './store'
+
+import './reset.css'
+import './styles.css'
+
+Modal.setAppElement('.app-root')
 
 ReactDOM.render(
-  <div>
-    <p>Hello, World</p>
-  </div>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.querySelector('.app-root')
 )
